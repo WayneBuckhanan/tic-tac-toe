558567333c07b027cc940a3ab6d67bdcb7139996
Alex Noguchi <alexander.noguchi@gmail.com>
2014-01-27 01:34:27 -0800

    Good enough for government work
    Ugly as sin, both code and visually. yay.

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,7 +1,7 @@
 // GAME STATE!
 // WARNING! GLOBAL VARIABLE MUTATION AHEAD!
 // Initialize a blank game
-var game = new Array(9);
+var game = [false,false,false,false,false,false,false,false,false];
 // Turn: 1 is red 2 is blue
 var turn = 1;
 
@@ -14,6 +14,11 @@ var turn_color = [,'red','blue']
 function cell_index(cell) { return $(cell).data("row")*3 + $(cell).data("col"); }
 // Toggles turn between 1 and 2
 function inc_turn(turn) { return turn % 2 + 1; }
+
+// returns true if the given index is a side else false
+function is_side(i) { return i%2 == 1; }
+// returns true if the given index is a corner else false
+function is_corner(i) { return i%2 == 0 && i != 4; }
 
 //////
 // Functions to check for game over conditions
@@ -47,24 +52,73 @@ function check_back_diag(i) {
     return i % 8 != 0 && i % 2 == 0 && game[2]==game[4] && game[4]==game[6];
 }
 
+// RANDOM FUNCTIONS!
+// No really, they generate random numbers. . .
+// Returns a random int >= 0 and < n
+// n -> [0,n)
+function random_int(n) { return n == 0 ? false : Math.floor(Math.random()*n); }
+// returns a random element in an array, a
+function random_element(a) { return a[random_int(a.length)]; }
+
+function empty_indices(a) { return a.map(each_empty_index).filter(is_not_false); }
+function each_empty_index(val, i) { return val ? false : i; }
+function is_not_false(v) { return !(v === false); }
+
+// Random options
+function free_space() { return random_element(empty_indices(game)); }
+function free_side() { return random_element(empty_indices(game).filter(is_side));}
+function free_corner() { return random_element(empty_indices(game).filter(is_corner));}
+function binary() { return random_int(2); }
+
+// Destructive functions!
+function play_at(i) {
+    // Change Game data
+    game[i] = turn;
+    
+    update_view(i);
+    if(is_game_over(i)) {
+        alert("Game Over!"+game+'\n'+is_game_over(i)+'\n'+check_col(i)+check_row(i)+check_diag(i));
+        location.reload();
+    }
+    else {
+        turn = inc_turn(turn);
+    }
+}
+function update_view(i) {
+    $("#box_"+((i - i%3)/3)+"_"+(i%3)).addClass(turn_color[turn]);
+}
+
+// EVENTS
 $(document).ready(function(){
     // Take the appropriate action when a box is clicked
     $('.box_cell').click(function(){
         var i = cell_index(this);
-        
         // If the cell is open
         if(!game[i]) {
-            // Change Game data
-            game[i] = turn;
-            // Change 'view'
-            $(this).addClass(turn_color[turn]);
-            if(is_game_over(i)) {
-                alert("Game Over!");
-                location.reload();
-            }
-            else {
-                inc_turn();
-            }
+            play_at(i);
         }
+    });
+    
+    // Buttons!
+    $('.play').click(function() {
+        console.log(this.id);
+        // OMG MAGIC! No, it's just hubris. . .
+        var i = window[this.id]();
+        console.log(i);
+        if(i === false || i === undefined) {
+            console.log('no go!');
+        } else {
+            play_at(i);
+        }
+    });
+    
+    $('#binary').click(function(){
+        var m = $('#message');
+        m.fadeOut({
+            complete: function(){
+                m.text(''+binary());
+                m.fadeIn();
+            }
+        });
     });
 });
